3b4c8bdd6b2c70c8ea31999dbd3b1d67f746b78d
Nikolay Sturm <240915+nistude@users.noreply.github.com>
2025-01-05 16:15:41 +0100

    add key override / custom shift

--- keyboards/zsa/voyager/keymaps/nistude/nistude.c
+++ keyboards/zsa/voyager/keymaps/nistude/nistude.c
@@ -87,3 +87,12 @@ combo_t key_combos[] = {
     COMBO(e_u_combo, M_E_GRAVE),
     COMBO(e_p_combo, M_E_ACUTE),
 };
+
+/*
+ * Key Overrides / Custom Shift (https://docs.qmk.fm/features/key_overrides)
+ */
+const key_override_t shft_bspc_del_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+const key_override_t *key_overrides[] = {
+	&shft_bspc_del_override,
+};
